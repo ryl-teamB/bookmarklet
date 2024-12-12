fb08dffa12a76893f2f9d866e194553dd262f659
sa-arao <arosts6180@gmail.com>
2024-12-12 16:56:33 +0900

    fix : クーポン有無判別条件を修正

--- get_skuinfo_rktn.js
+++ get_skuinfo_rktn.js
@@ -447,7 +447,8 @@ async function getRakutenInfo() {
 	// クーポン情報を取得する
 	const result = await fetchCouponData(itemInfo, minPrice);
 	const resultJson = JSON.parse(result);
-	const couponData = resultJson.items[0].length > 0 ? resultJson.items[0] : null;
+	// alert(resultJson);
+	const couponData = resultJson.items && resultJson.items[0] && resultJson.items[0].length > 0 ? resultJson.items[0] : null;
 	// alert(couponData);
 
 	const couponBaseUrl = 'https://coupon.rakuten.co.jp/getCoupon?getkey=';
